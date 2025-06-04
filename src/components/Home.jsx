7aab39d92e08e743f8bc0340636b50a8e8106c25
virajpate001 <virajpate001@gmail.com>
2025-06-04 21:21:25 +0530

    problemm-added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from './Hero'
+import Problem from './Problem'
 
 const Home = () => {
   return (
     <div>
       <Hero/>
+      <Problem/>
     </div>
   )
 }
---
+++ src/components/Problem.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Problem = () => {
+  return (
+    <section>
+      <h3>Problem</h3>
+    </section>
+  )
+}
+
+export default Problem
